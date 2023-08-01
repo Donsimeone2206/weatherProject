e20aca5ea436740592b165108323853c23e7a3bf
Simeon A. Mustapha <simeon.mustapha@gmail.com>
2023-08-01 14:16:58 +0100

    Weather based Project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+
+app.get("/", function(req, res) {
+    res.send
+})
+
+
+
+
+app.listen(3000, function() {
+    console.log("server is running")
+})
